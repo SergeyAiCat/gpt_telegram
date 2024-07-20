c428e3ee33ef3259d87a24211193293ab927f261
SergeyAiCat <vzasil2341@gmail.com>
2024-07-20 16:58:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 #openai токен
-openai_key="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"
+openai_key="******"
 #телеграм токен
-TOKEN = "7037636667:AAGBFBYASR7L97ivB0FP8b52qF3CREeY6O8"
+TOKEN = "******"
